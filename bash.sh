8350c39ff741049c99d34cad38176b92dc5d1223
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-05-28 16:28:09 +0000

    IFS ga simple example qildim.

--- bash.sh
+++ bash.sh
@@ -2,7 +2,6 @@
 
 # IFS read for ifs.txt
 
-while read -r line; do
-    IFS=',' read -ra fields <<< "$line"
-    echo "${fields[0]}  ${fields[1]}  ${fields[2]}  ${fields[3]}"
+while IFS= read -r line; do
+    echo "$line"
 done < ifs.txt
