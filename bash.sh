9a7b3ac7286010399a206377a8a6430b624eb1ce
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-05-28 16:58:26 +0000

    declare orqalik Xatolik! (readonly) ga example bo'ldi

--- bash.sh
+++ bash.sh
@@ -2,5 +2,7 @@
 
 # declare of the script
 
-declare myvar="Salom"
-echo "$myvar"
+declare -r pi=3.14
+echo "The value of pi is: $pi"
+pi=5 # This will not change the value of pi
+echo "The value of pi is still: $pi"
