bc102488d2cb5abba5cc7ca18deb9bb98421710e
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-05-28 17:08:02 +0000

    declare -A nums ga example bo'ldi

--- bash.sh
+++ bash.sh
@@ -2,7 +2,31 @@
 
 # declare of the script
 
-declare -a arr=("a" "b" "c")
-echo ${arr[0]}
-echo ${arr[1]}
-echo ${arr[2]}
+declare -A nums
+
+nums=(
+    ["1"]="bir"
+    ["2"]="ikki"
+    ["3"]="uch"
+    ["4"]="to'rt"
+    ["5"]="besh"
+    ["6"]="olti"
+    ["7"]="yetti"
+    ["8"]="sakkiz"
+    ["9"]="to'qqiz"
+)
+
+printf "Raqamni kiriting (1-9): "
+read -r num
+
+# check if the input is a valid number
+if ! [[ "$num" =~ ^[1-9]$ ]]; then
+    echo "Iltimos, 1 dan 9 gacha bo'lgan raqam kiriting."
+    exit 1
+fi
+# check if the number exists in the associative array
+if [[ -z "${nums[$num]}" ]]; then
+    echo "Noto'g'ri raqam kiritdingiz!"
+else
+    echo "Siz kiritgan raqam: ${nums[$num]}"
+fi
