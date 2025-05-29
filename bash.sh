4e2141ff08f8ad693854264e9ae0bbb0c1fe0def
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-05-29 14:37:49 +0000

    Arraydan element o‘chirish

--- bash.sh
+++ bash.sh
@@ -6,7 +6,24 @@ my_array=(apple banana cherry)
 my_array+=(orange grape)
 count=1
 
+echo "Olding array elements"
+
 for fruit in "${my_array[@]}"; do
   echo "Meva ${count} $fruit"
     ((count++))
 done
+
+echo "-------------------------"
+count=1
+
+echo "Keyin array elements"
+
+unset my_array[0]  # Remove 'banana' from the array
+
+for fruit in "${my_array[@]}"; do
+  echo "Meva ${count} $fruit"
+    ((count++))
+done
+
+# End of the script
+exit 0
