19468bb4094c2ee542a53cd3f6f1ddf06eb5d985
Abdurahim <rixsiboyevabdurahim32@gmail.com>
2025-05-29 14:42:01 +0000

    Indekslari bilan chiqarish

--- bash.sh
+++ bash.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-# declare of the script
+mevalar=(olma anor uzum)
 
-my_array=(apple banana cherry)
-my_array+=(orange grape)
-count=1
 
-echo "Olding array elements"
-
-for fruit in "${my_array[@]}"; do
-  echo "Meva ${count} $fruit"
-    ((count++))
+for i in "${!mevalar[@]}"; do
+    echo "Meva: ${mevalar[$i]}"
 done
-
-echo "-------------------------"
-count=1
-
-echo "Keyin array elements"
-
-unset my_array[0]  # Remove 'banana' from the array
-
-for fruit in "${my_array[@]}"; do
-  echo "Meva ${count} $fruit"
-    ((count++))
-done
-
-# End of the script
-exit 0
